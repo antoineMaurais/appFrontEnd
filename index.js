7ced3e5efe1ac87d41adb523b9af0b8ee6931be3
antoineMaurais <antoine.maurais98@gmail.com>
2023-12-22 15:51:10 +0100

    Update comment

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-const express = require('express');
-const mongodb = require('mongodb');
-const path = require('path');
+// Importation des modules nécessaires
+const express = require('express'); // Framework web pour Node.js
+const mongodb = require('mongodb'); // Driver MongoDB pour Node.js
+const path = require('path'); // Module pour manipuler les chemins de fichiers
 
+// Initialisation de l'application Express
 const app = express();
-const port = 3000;
+const port = 3000; // Port sur lequel le serveur va écouter
 
+// Informations de connexion à la base de données MongoDB
 const mongoUser = 'admin';
 const mongoPassword = 'admin';
 const mongoHost = '192.168.100.102';
 const mongoPort = '27017';
 const mongoDatabase = 'dbStudent';
 
+// Construction de l'URL de connexion à MongoDB
 const mongoConnection = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}`;
 
+// Création d'une instance du client MongoDB
 const mongClient = new mongodb.MongoClient(mongoConnection);
 
+// Données initiales d'étudiants à insérer dans la base de données
 const studentsData = [
   { firstName: 'John', lastName: 'Doe', className: 'A' },
   { firstName: 'Jane', lastName: 'Smith', className: 'B' },
   { firstName: 'Bob', lastName: 'Johnson', className: 'A' }
 ];
 
+// Fonction asynchrone pour initialiser la base de données
 async function initDB(req, res) {
   try {
-    await mongClient.connect();
+    await mongClient.connect(); // Connexion à la base de données
     let database = mongClient.db(mongoDatabase);
     let students = database.collection('students');
     let studentsCount = await students.countDocuments();
 
+    // Vérification si la base de données est vide, puis insertion des données initiales
     if (studentsCount === 0) {
       await students.insertMany(studentsData);
     }
@@ -36,17 +44,19 @@ async function initDB(req, res) {
   }
 }
 
+// Utilisation du middleware pour analyser les données JSON dans les requêtes
 app.use(express.json());
 
-// Serve the React app build
+// Serveur des fichiers statiques de l'application React
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Endpoint POST pour ajouter un étudiant à la base de données
 app.post('/api/students', async (req, res) => {
   const { firstName, lastName, className } = req.body;
   const student = { firstName, lastName, className };
 
   try {
-    await mongClient.connect(); // Connectez-vous si la connexion n'est pas déjà établie
+    await mongClient.connect(); // Connexion à la base de données si nécessaire
     let database = mongClient.db(mongoDatabase);
     await database.collection('students').insertOne(student);
     res.status(201).send(student);
@@ -55,9 +65,10 @@ app.post('/api/students', async (req, res) => {
   }
 });
 
+// Endpoint GET pour récupérer tous les étudiants de la base de données
 app.get('/api/students', async (req, res) => {
   try {
-    await mongClient.connect(); // Connectez-vous si la connexion n'est pas déjà établie
+    await mongClient.connect(); // Connexion à la base de données si nécessaire
     let database = mongClient.db(mongoDatabase);
     const students = await database.collection('students').find({}).toArray();
     res.send(students);
@@ -66,7 +77,7 @@ app.get('/api/students', async (req, res) => {
   }
 });
 
-// Handle other routes by serving the React app
+// Gestion des autres routes en servant l'application React
 app.get('*', (req, res) => {
   initDB(req, res);
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -78,6 +89,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something went wrong!');
 });
 
+// Lancement du serveur sur le port spécifié
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
